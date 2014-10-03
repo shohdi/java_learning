c0eba938694f5c1bd59046c8cd2c749571410ba0
shohdi <shohdi@gmail.com>
2014-10-03 22:42:15 +0200

    first program

---
+++ Chapter1/src/Welcome.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * 
+ * 
+ * @version 1.0 3/10/2014
+ * @author shohdi elsheemy
+ */
+public class Welcome
+{
+    public static void main(String[] args)
+    {
+        String[] greating = new String[3];
+        greating[0] = "this is first program";
+        greating[1] = "Hi";
+        greating[2] = "eid mubarak";
+        
+        for (String gr : greating)
+        {
+            System.out.println(gr);
+        }
+                
+                
+    }
+}
